d39ffd9b860029756014760449f18be7de7e5a4e
karainsun <1210388155@qq.com>
2021-10-19 13:42:51 +0800

    修改mysql配置为生产

--- server/config/sequelizeConfig.js
+++ server/config/sequelizeConfig.js
@@ -1,10 +1,10 @@
 const config = {
   dialect: "mysql",
-  host: "localhost",
+  host: "152.32.173.232",
   port: 3306,
   userName: "root",
   password: "root",
-  databaseName: "blog_sql",
+  databaseName: "mysql",
   ssl: false,
   timezone: "+08:00"
 }

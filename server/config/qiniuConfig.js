a660dbf4c5e4ef4a4cd293e217ef289d2d4b3aa9
karainsun <1210388155@qq.com>
2021-10-21 08:54:08 +0800

    修改七牛云域名

--- server/config/qiniuConfig.js
+++ server/config/qiniuConfig.js
@@ -2,6 +2,6 @@ const QINIU = {
   accessKey: "I0oHgt9jNgJ_PtoBKIK_py9JfikXRkjHw9I5Wpsh",
   secretKey: "A9MRXEUM2eXnSuF7MnEV6oaFYjbRNzuu-_c0LDAM",
   bucket: "kay-oss2",
-  origin: "kayrain.cn",
+  origin: "cdn.kayrain.cn",
 };
 module.exports = QINIU

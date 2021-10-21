56918beefc6115d84c54ea737db92c873b226c3d
karainsun <1210388155@qq.com>
2021-10-21 08:13:26 +0800

    配置七牛域名

--- server/config/qiniuConfig.js
+++ server/config/qiniuConfig.js
@@ -2,6 +2,6 @@ const QINIU = {
   accessKey: "I0oHgt9jNgJ_PtoBKIK_py9JfikXRkjHw9I5Wpsh",
   secretKey: "A9MRXEUM2eXnSuF7MnEV6oaFYjbRNzuu-_c0LDAM",
   bucket: "kay-oss2",
-  origin: "qyybgf4om.hn-bkt.clouddn.com",
+  origin: "kayrain.cn",
 };
 module.exports = QINIU
